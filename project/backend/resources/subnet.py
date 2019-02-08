15dde91f4c30e0279b3af36121fce49313189c68
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-02-08 18:07:42 +0100

    backend: Done POST request

--- project/backend/resources/subnet.py
+++ project/backend/resources/subnet.py
@@ -3,30 +3,6 @@ from datetime import datetime
 import json
 
 subnets = {
-    'subnet1': {
-        "id": 1,
-        "ip": "203.0.13.0/32",
-        "next_hop": "192.0.2.1",
-        "communities": [
-            "564:65"
-        ],
-        "created_at" : "Fri, 5 Feb 2019 09:46:56 GMT",
-        "modified_at": "null",
-        "is_activated": False,
-        "last_activation": "Fri, 5 Feb 2019 09:47:00 GMT",
-    },
-    'subnet2': {
-        "id": 2,
-        "ip": "3.0.13.0/32",
-        "next_hop": "192.0.2.1",
-        "communities": [
-            "56:655"
-        ],
-        "created_at": "Fri, 5 Feb 2019 09:50:00 GMT",
-        "modified_at": "null",
-        "is_activated" : True,
-        "last_activation": "Fri, 5 Feb 2019 09:53:00 GMT",
-    },
 }
 
 subnets_fields = {
@@ -34,10 +10,10 @@ subnets_fields = {
     'ip': fields.String,
     'next_hop': fields.String,
     'communities': fields.List(fields.String),
-    'created_at': fields.DateTime,
-    'modified_at': fields.DateTime,
+    'created_at': fields.String,
+    'modified_at': fields.String,
     'is_activated': fields.Boolean,
-    'last_activation': fields.DateTime,
+    'last_activation': fields.String,
 }
 
 post_parser = reqparse.RequestParser()
@@ -53,24 +29,27 @@ post_parser.add_argument(
     "communities", dest="communities", location=["form", "json"], 
     required=True, help="The community",
 )
+
 class Subnet(Resource):
     def get(self):
-        return json.dumps(subnets)
+        return subnets
     
     @marshal_with(subnets_fields)
     def post(self):
         args = post_parser.parse_args()
-        last_subnet = max(subnets.keys())
-        id = subnets[last_subnet]['id'] + 1
+        id = 1
+        if (len(subnets.keys()) > 0):
+            last_subnet = max(subnets.keys())
+            id = subnets[last_subnet]['id'] + 1
         subnet_name = "subnet%i" % id
         subnets[subnet_name] = {
             'id': id,
             'ip': args.ip,
             'next_hop': args.next_hop,
             'communities': [args.communities],
-            'created_at': datetime.now(),
-            'modified_at': datetime.now(),
+            'created_at': str(datetime.now()),
+            'modified_at': str(datetime.now()),
             'is_activated': True,
-            'last_activation': datetime.now(),
+            'last_activation': str(datetime.now()),
         }
         return subnets[subnet_name], 201
