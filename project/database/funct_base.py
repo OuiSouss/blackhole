2c44e0ef9db4d4edd1c4bec36829ce81af90f2b5
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-02-08 21:10:32 +0100

    project : database connection and management with console

--- project/database/funct_base.py
+++ project/database/funct_base.py
@@ -1,15 +1,18 @@
 from pymongo import MongoClient
-import datetime
+from datetime import *
+import pprint
+#from urllib import request, parse
+#import json
 
-client = MongoClient('localhost:27017')
+uri = 'mongodb://Bidule45:Truc45@ds127115.mlab.com:27115/route_base'
+
+client = MongoClient(uri)
 global dbr
-global dbi
-dbr = client.Route
-dbi = client.Security
+dbr = client.route_base
+print(dbr.collection_names(include_system_collections=False))
 
 def insert():
     try:
-        routeId = input('Enter Id :')
         routeNetwork = input('Enter Network :')
         routeMask = input('Enter Mask :')
         routeNextHop = input('Enter NextHop :')
@@ -17,67 +20,91 @@ def insert():
         
         dbr.Route.insert_one(
             {
-                "id": routeId,
-                "network": routeNetwork,
-                "mask": routeMask,
-                "nexthop": routeNextHop,
-                "community": routeCommunity,
-                "enabled": 1,
-     #           "created":datetime.now(),
-     #           "modified":datetime.now(),
+                "network":routeNetwork,
+                "mask":routeMask,
+                "nexthop":routeNextHop,
+                "community":routeCommunity,
+                "enabled":1,
+                "created":datetime.utcnow(),
+                "modified":datetime.utcnow(),
+                "last_activation":datetime.utcnow()
         })
         print ('\nInserted data successfully\n')
     
     except Exception as e:
-        print (str(e))
+        print (e)
 
 def read():
     try:
         routesCol = dbr.Route.find()
         print ('\n All data from Route Database \n')
         for route in routesCol:
-            print (route)
+            pprint.pprint(route)
 
     except Exception as e:
-        print (str(e))
+        print (e)
 
+def read_one():
+    try:
+        routeNetwork = input('Enter Network :')
+        route = dbr.Route.find_one({"network":routeNetwork})
+        print ('\n Data from Route Database \n')
+        pprint.pprint(route)
+
+    except Exception as e:
+        print (e)
+
+def get():
+    while(1):
+        selection = input('\nSelect RALL to read all data, RONE to read one or Q to quit\n')
+
+        if selection == 'RALL':
+            read()
+        elif selection == 'RONE':
+            read_one()
+        elif selection == 'Q':
+            print('\nExiting Read option\n')
+            break
+        else:
+            print ('\n INVALID SELECTION \n')
+
+    
 def activate():
     try:
-        criteria = input('\nEnter id to update\n')
         routeNetwork = input('\nEnter network to update\n')
 
         dbr.Route.update_one(
-            {"id": criteria},
+            {"network": routeNetwork},
             {
                 "$set": {
-      #              "modified":datetime.now(),
-                    "enabled":0
-                }
-            }
-        )
-        print ("\nRecord updated successfully\n")   
-    
-    except Exception as e:
-        print (str(e))
-
-def desactivate():
-    try:
-        criteria = input('\nEnter id to update\n')
-        qrouteNetwork = input('\nEnter network to update\n')
-
-        dbr.Route.update_one(
-            {"id": criteria},
-            {
-                "$set": {
-       #             "modified":datetime.now(),
+                    "modified":datetime.utcnow(),
+                    "last_activation":datetime.utcnow(),
                     "enabled":1
                 }
             }
         )
-        print ("\nRecord updated successfully\n")
+        print ('\nRecord updated successfully\n')    
     
     except Exception as e:
-        print (str(e))
+        print (e)
+
+def desactivate():
+    try:
+        routeNetwork = input('\nEnter network to update\n')
+
+        dbr.Route.update_one(
+            {"network": routeNetwork},
+            {
+                "$set": {
+                    "modified":datetime.utcnow(),
+                    "enabled":0
+                }
+            }
+        )
+        print ('\nRecord updated successfully\n')    
+    
+    except Exception as e:
+        print (e)
 
 def update():
     selection = input('\nSelect ACT to activate, DESACT to desactivate\n')
@@ -92,64 +119,66 @@ def update():
 
 def delete():
     try:
-        criteria = input('\nEnter id to delete\n')
-        dbr.Route.delete_many({"id": criteria})
-        print ('\nDeletion successful\n')
+        routeNetwork = input('\nEnter network to delete\n')
+        dbr.Route.delete_many({"network": routeNetwork})
+        print ('\nDeletion successful\n') 
     except Exception as e:
-        print (str(e))
+        print (e)
 
-def register():
-    try:
-        userId = input('Enter Id :')
-        userUsername = input('Enter Username :')
-        userPassword = input('Enter Password :')
-        userEmail = input('Enter Email :')
-        
-        dbi.Security.insert_one(
-            {
-                "id": userId,
-                "username":userUsername,
-                "password":userPassword,
-                "email":userEmail,
-     #           "created":datetime.now(),
-     #           "modified":datetime.now(),
-        })
-        print ('\nRegistered successfully\n')
-    
-    except Exception as e:
-        print (str(e))
-
-def connect():
-    while(1):
-        selection = input('\nSelect POST to insert, PUT to update, to read, DELETE to delete, DECO to disconnect\n')
-        if selection == 'POST':
-            insert()
-        elif selection == 'PUT':
-            update()
-        elif selection == 'GET':
-           	read()
-        elif selection == 'DELETE':
-           	delete()
-        elif selection == 'DECO':
-            print ('\nBye\n')
-            break
-        else:
-            print ('\n INVALID SELECTION \n')
-
-def login():
-    try:
-        usersCol = dbi.Security.find()
-        #userId = raw_input('Enter Id :')
-        userUsername = input('Enter Username :')
-        userPassword = input('Enter Password :')
-        for users in usersCol:
-            if users == userUsername:
-                if users.password == userPassword:
-                    connect()
-                else:
-                    print ('\nBad password\n')
-            else:
-                print ('\nBad username\n')
-
-    except Exception as e:
-        print (str(e))
+##def register():
+##    try:
+##        userId = raw_input('Enter Id :')
+##        userUsername = raw_input('Enter Username :')
+##        userPassword = raw_input('Enter Password :')
+##        userEmail = raw_input('Enter Email :')
+##        
+##        dbi.Security.insert_one(
+##            {
+##                "id": userId,
+##                "username":userUsername,
+##                "password":userPassword,
+##                "email":userEmail,
+##     #           "created":datetime.now(),
+##     #           "modified":datetime.now(),
+##        })
+##        print '\nRegistered successfully\n'
+##    
+##    except Exception, e:
+##        print str(e)
+##
+##def connect():
+##	while(1):
+##        	selection = raw_input('\nSelect POST to insert, PUT to update,  GET to read, DELETE to delete, DECO to disconnect\n')
+##
+##    		if selection == 'POST':
+##        		insert()
+##    		elif selection == 'PUT':
+##       			update()
+##    		elif selection == 'GET':
+##        		read()
+##    		elif selection == 'DELETE':
+##        		delete()
+##    		elif selection == 'DECO':
+##			print '\nBye\n'
+##			break
+##    		else:
+##        		print '\n INVALID SELECTION \n'
+##
+##def login():
+##    try:
+##        usersCol = dbi.Security.find()
+##         #userId = raw_input('Enter Id :')
+##	userUsername = raw_input('Enter Username :')
+##	userPassword = raw_input('Enter Password :')
+##        for users in usersCol:
+##            if users == userUsername:
+##		if users.password == userPassword:
+##			connect()
+##		else:
+##			print '\nBad password\n'
+##	    else:
+##			print '\nBad username\n'
+##
+##    except Exception, e:
+##        print str(e)
+##
--- project/database/start_base.py
+++ project/database/start_base.py
@@ -2,14 +2,30 @@
 from funct_base import *
 import sys
 
+##while(1):
+##    selection = raw_input('\nSelect C to connect, R to register, Q to quit\n')
+##    if selection =='R':
+##	register()
+##    elif selection == 'C':
+##    	login()
+##    elif selection == 'Q':
+##	print '\nBye bye\n'
+##	sys.exit(1)
+##    else:
+##        print '\n INVALID SELECTION \n'
 while(1):
-    selection = input('\nSelect C to connect, R to register, Q to quit\n')
-    if selection =='R':
-	    register()
-    elif selection == 'C':
-    	login()
+    selection = input('\nSelect POST to insert, PUT to update,  GET to read, DEL to delete or Q to quit\n')
+
+    if selection == 'POST':
+        insert()
+    elif selection == 'PUT':
+        update()
+    elif selection == 'GET':
+        get()
+    elif selection == 'DEL':
+        delete()
     elif selection == 'Q':
-	    print ('\nBye bye\n')
-	    sys.exit(1)
+        print ('\nBye\n')
+        sys.exit(1)
     else:
         print ('\n INVALID SELECTION \n')
