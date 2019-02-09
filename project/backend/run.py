5019f94db354df1e677b17096ab2e1274aeb6b83
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-02-09 14:10:13 +0100

    backend : Use werzeug serving

--- project/backend/run.py
+++ project/backend/run.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from werkzeug.serving import run_simple
 
 def create_app():
     app = Flask(__name__)
-
+    app.debug = True
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
@@ -11,4 +12,5 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    run_simple('localhost', 5000, app,
+               use_reloader=True, use_debugger=True, use_evalex=True)
