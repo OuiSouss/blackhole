a69301b40c63255041fdee8b073ad579f7a4b162
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-02-09 17:19:56 +0100

    backend : Remove werzeug serving

--- project/backend/run.py
+++ project/backend/run.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from werkzeug.serving import run_simple
 
 def create_app():
     app = Flask(__name__)
-    app.debug = True
+
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
@@ -12,5 +11,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    run_simple('localhost', 5000, app,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+    app.run(debug=True)
