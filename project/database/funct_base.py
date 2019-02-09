7770843cf4aef8218b8d6eff3dff2f9eed125bd7
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-02-09 14:40:58 +0100

    database : Reformate code and remove unused code

--- project/database/funct_base.py
+++ project/database/funct_base.py
@@ -1,8 +1,6 @@
 from pymongo import MongoClient
-from datetime import *
+from datetime import datetime
 import pprint
-#from urllib import request, parse
-#import json
 
 uri = 'mongodb://Bidule45:Truc45@ds127115.mlab.com:27115/route_base'
 
@@ -20,14 +18,14 @@ def insert():
         
         dbr.Route.insert_one(
             {
-                "network":routeNetwork,
-                "mask":routeMask,
-                "nexthop":routeNextHop,
-                "community":routeCommunity,
-                "enabled":1,
-                "created":datetime.utcnow(),
-                "modified":datetime.utcnow(),
-                "last_activation":datetime.utcnow()
+                "network": routeNetwork,
+                "mask": routeMask,
+                "nexthop": routeNextHop,
+                "community": routeCommunity,
+                "enabled": 1,
+                "created": datetime.utcnow(),
+                "modified": datetime.utcnow(),
+                "last_activation": datetime.utcnow()
         })
         print ('\nInserted data successfully\n')
     
@@ -47,7 +45,7 @@ def read():
 def read_one():
     try:
         routeNetwork = input('Enter Network :')
-        route = dbr.Route.find_one({"network":routeNetwork})
+        route = dbr.Route.find_one({"network": routeNetwork})
         print ('\n Data from Route Database \n')
         pprint.pprint(route)
 
@@ -77,9 +75,9 @@ def activate():
             {"network": routeNetwork},
             {
                 "$set": {
-                    "modified":datetime.utcnow(),
-                    "last_activation":datetime.utcnow(),
-                    "enabled":1
+                    "modified": datetime.utcnow(),
+                    "last_activation": datetime.utcnow(),
+                    "enabled": 1
                 }
             }
         )
@@ -96,8 +94,8 @@ def desactivate():
             {"network": routeNetwork},
             {
                 "$set": {
-                    "modified":datetime.utcnow(),
-                    "enabled":0
+                    "modified": datetime.utcnow(),
+                    "enabled": 0
                 }
             }
         )
@@ -124,61 +122,3 @@ def delete():
         print ('\nDeletion successful\n') 
     except Exception as e:
         print (e)
-
-##def register():
-##    try:
-##        userId = raw_input('Enter Id :')
-##        userUsername = raw_input('Enter Username :')
-##        userPassword = raw_input('Enter Password :')
-##        userEmail = raw_input('Enter Email :')
-##        
-##        dbi.Security.insert_one(
-##            {
-##                "id": userId,
-##                "username":userUsername,
-##                "password":userPassword,
-##                "email":userEmail,
-##     #           "created":datetime.now(),
-##     #           "modified":datetime.now(),
-##        })
-##        print '\nRegistered successfully\n'
-##    
-##    except Exception, e:
-##        print str(e)
-##
-##def connect():
-##	while(1):
-##        	selection = raw_input('\nSelect POST to insert, PUT to update,  GET to read, DELETE to delete, DECO to disconnect\n')
-##
-##    		if selection == 'POST':
-##        		insert()
-##    		elif selection == 'PUT':
-##       			update()
-##    		elif selection == 'GET':
-##        		read()
-##    		elif selection == 'DELETE':
-##        		delete()
-##    		elif selection == 'DECO':
-##			print '\nBye\n'
-##			break
-##    		else:
-##        		print '\n INVALID SELECTION \n'
-##
-##def login():
-##    try:
-##        usersCol = dbi.Security.find()
-##         #userId = raw_input('Enter Id :')
-##	userUsername = raw_input('Enter Username :')
-##	userPassword = raw_input('Enter Password :')
-##        for users in usersCol:
-##            if users == userUsername:
-##		if users.password == userPassword:
-##			connect()
-##		else:
-##			print '\nBad password\n'
-##	    else:
-##			print '\nBad username\n'
-##
-##    except Exception, e:
-##        print str(e)
-##
