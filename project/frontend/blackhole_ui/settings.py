ee357436e1bcdad8a07d5f45cde5c5b14cbe05cf
Kevin Maitre <kevin.maitre@etu.u-bordeaux.fr>
2019-02-09 22:42:25 +0100

    frontend : GET route manager

--- project/frontend/blackhole_ui/settings.py
+++ project/frontend/blackhole_ui/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'route_manager.apps.RouteManagerConfig',
 ]
 
 MIDDLEWARE = [
--- project/frontend/blackhole_ui/views.py
+++ project/frontend/blackhole_ui/views.py
@@ -2,16 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template import loader
 from django.shortcuts import render
+import requests
+import json
 
 def index(request):
     if request.user.is_authenticated:
-        #return HttpResponse("Gestion des routes")
+        response = requests.get('http://127.0.0.1:5000/api/subnet')
+        json_data = response.json()
 
+        #return HttpResponse("Gestion des routes")
         #template = loader.get_template('templates/route_manager.html')
         #return HttpResponse(template.render())
         #return render(request, 'templates/route_manager.html', context)
-
-        return render(request,'route_dashboard/route_dashboard.html')
-    
+        
+        
+        return render(request, 'route_dashboard/route_dashboard.html', {
+            'data' : json_data,
+        })
     else:
         return redirect('/accounts/login/')
