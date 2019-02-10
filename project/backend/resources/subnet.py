cb98dae1f22af4cdce1e44a9515f0f760a8eada2
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-02-10 16:03:03 +0100

    backend : Add DELETE method and docstring

--- project/backend/resources/subnet.py
+++ project/backend/resources/subnet.py
@@ -1,5 +1,8 @@
-from flask_restful import Resource, reqparse, fields, marshal_with
+"""
+REST API /api/subnet
+"""
 from datetime import datetime
+from flask_restful import Resource, reqparse, fields, marshal_with
 from flask import jsonify
 
 subnets = []
@@ -15,32 +18,68 @@ subnets_fields = {
     'last_activation': fields.String,
 }
 
-post_parser = reqparse.RequestParser()
-post_parser.add_argument(
-    "ip", dest="ip", location=["form", "json"], required=True, help="The IP",
-)
-post_parser.add_argument(
-    "next_hop", dest="next_hop", location=["form", "json"], required=True, 
-    help="The next hop",
-)
+def create_post_parser():
+    """
+    Create a parser for POST request which needs
+    an IP, the next_hop, and communities on which we need
+    to apply the route
+    :return: post_parser
+    """
+    post_parser = reqparse.RequestParser()
+    post_parser.add_argument(
+        "ip", dest="ip", location=["form", "json"], required=True,
+        help="The IP",
+    )
+    post_parser.add_argument(
+        "next_hop", dest="next_hop", location=["form", "json"], required=True,
+        help="The next hop",
+    )
 
-post_parser.add_argument(
-    "communities", dest="communities", location=["form", "json"], 
-    required=True, help="The community", action="append"
-)
+    post_parser.add_argument(
+        "communities", dest="communities", location=["form", "json"],
+        required=True, help="The community", action="append"
+    )
+    return post_parser
+
+def create_delete_parser():
+    """
+    Create a parser for DELETE request which need a ID of a resource
+    :return: delete_parser
+    """
+    delete_parser = reqparse.RequestParser()
+    delete_parser.add_argument(
+        "id", dest="id", location=["form", "json"], required=True,
+        help="The ID",
+    )
+    return delete_parser
 
 class Subnet(Resource):
+    """
+    Subnet class to provide GET, POST, PUT, DELETE and PATCH methods to
+    frontend. This class communicates with ExaBGP and store info in MongoDB
+    database.
+    """
+
     def get(self):
+        """
+        GET request
+        :return: List of subnets in json stored
+        """
         return jsonify(subnets)
-    
+
     @marshal_with(subnets_fields)
     def post(self):
-        args = post_parser.parse_args()
-        id = 1
-        if (len(subnets) > 0):
-            id = subnets[-1]['id'] + 1
+        """
+        POST request create a new subnet
+        It will announced to ExaBGP and store in database
+        :return: The new subnet with 201 status
+        """
+        args = create_post_parser().parse_args()
+        id_subnet = 1
+        if subnets:
+            id_subnet = subnets[-1]['id'] + 1
         subnet = {
-            'id': id,
+            'id': id_subnet,
             'ip': args.ip,
             'next_hop': args.next_hop,
             'communities': args.communities,
@@ -51,3 +90,15 @@ class Subnet(Resource):
         }
         subnets.append(subnet)
         return subnet, 201
+
+    @marshal_with(subnets_fields)
+    def delete(self):
+        """
+        DELETE request delete a subnet which has a specific ID
+        """
+        args = create_delete_parser().parse_args()
+        for s in subnets:
+            if s["id"] == int(args.id):
+                subnets.remove(s)
+                return {"message": "Success"}, 204
+        return 404
