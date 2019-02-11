cdf9a6ec01fb4f99e25477c086ef0f0c1a4963fc
Geoffroy Gressier <ggressier@emi.u-bordeaux1.fr>
2019-02-11 18:22:05 +0100

    Possibilité de changer de mot de passe, meilleur gestion des utilisateurs authentifiés/pas authentifiés, meilleur routage à la racine, mise en place d'un mini modele de DEBUG, mise en pause du système d'affichage conditionnel des options

--- project/frontend/blackhole_ui/views.py
+++ project/frontend/blackhole_ui/views.py
@@ -1,12 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect
 from django.template import loader
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+
+from django.conf import settings
+
 import requests
 import json
 from route_manager.forms import PostForm
 
+
+
+#VERIFIE L'AUTHENTIFICATION
+def not_auth(request):
+    return (not request.user.is_authenticated)
+
+
+# '' => '/dashboard/'
+# '' => '/accounts/login/'
+def home(request):
+    
+    # AUTHENTIFICATION
+    if(not_auth(request)):
+        return redirect( settings.AUTH_URL )
+    # AUTHENTIFICATION
+    
+    return redirect( settings.LOGIN_REDIRECT_URL )
+
+
+# 'dashboard/'
 def index(request):
+
+    # AUTHENTIFICATION
+    if(not_auth(request)):
+        return redirect( settings.AUTH_URL )
+    # AUTHENTIFICATION
+    
     if request.user.is_authenticated:
         response = requests.get('http://127.0.0.1:5000/api/subnet')
         json_data = response.json()
@@ -28,6 +59,34 @@ def index(request):
         })
     else:
         return redirect('/accounts/login/')
+
+
+# 'accounts/change_password/'
+def change_password(request):
+
+    # AUTHENTIFICATION
+    if(not_auth(request)):
+        return redirect( settings.AUTH_URL )
+    # AUTHENTIFICATION
+    
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
+
+
+
+
 
 
 
