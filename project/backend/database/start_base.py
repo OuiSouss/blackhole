b215bf93ff0ae8c474342793e8f5a3f43c0acb22
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-02-12 16:52:46 +0100

    database : update main for management on console

--- project/backend/database/start_base.py
+++ project/backend/database/start_base.py
@@ -1,15 +1,29 @@
-
-from funct_base import *
 import sys
+from funct_base import MongoDB
+
 
 while(1):
-    selection = input('\nSelect C to connect, R to register, Q to quit\n')
-    if selection =='R':
-	    register()
-    elif selection == 'C':
-    	login()
+    self = MongoDB('Route')
+    selection = input('POST : add route, PATCH : update route, GET : get routes, DEL : delete route, Q : quit\n')
+
+    if selection == 'POST':
+        ip = input('Enter ip\n')
+        nexthop = input('Enter nexthop\n')
+        commu = input('Enter community\n')
+        post = {'ip': ip, 'next_hop': nexthop, 'communities': commu}
+        self.add_route(post)
+    elif selection == 'PATCH':
+        ip = input('Enter ip\n')
+        post = {'ip': ip}
+        self.update_route(post)    
+    elif selection == 'GET':
+        self.get_all_routes()
+    elif selection == 'DEL':
+        ip = input('Enter ip\n')
+        post = {'ip': ip}
+        self.delete_route(post)
     elif selection == 'Q':
-	    print ('\nBye bye\n')
-	    sys.exit(1)
+        print('\nBye\n')
+        sys.exit(1)
     else:
-        print ('\n INVALID SELECTION \n')
+        print('\n INVALID SELECTION \n')
