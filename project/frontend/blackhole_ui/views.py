dad61e4da09c7f65e1b6ca72ca4d34d1fc26047a
Kevin Maitre <kevin.maitre@etu.u-bordeaux.fr>
2019-02-12 15:42:43 +0100

    frontend : POST (finished) + try/catch error503 (server down)

--- project/frontend/blackhole_ui/views.py
+++ project/frontend/blackhole_ui/views.py
@@ -1,58 +1,56 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
-
 from django.conf import settings
-
-import requests
-import json
 from route_manager.forms import PostForm
+from requests.exceptions import ConnectionError
+import requests
 
 
 
 #VERIFIE L'AUTHENTIFICATION
 def not_auth(request):
-    return (not request.user.is_authenticated)
+    return not request.user.is_authenticated
 
 
 # '' => '/dashboard/'
 # '' => '/accounts/login/'
 def home(request):
-    
+
     # AUTHENTIFICATION
-    if(not_auth(request)):
-        return redirect( settings.AUTH_URL )
+    if not_auth(request):
+        return redirect(settings.AUTH_URL)
     # AUTHENTIFICATION
-    
-    return redirect( settings.LOGIN_REDIRECT_URL )
+
+    return redirect(settings.LOGIN_REDIRECT_URL)
 
 
 # 'dashboard/'
 def index(request):
 
     # AUTHENTIFICATION
-    if(not_auth(request)):
-        return redirect( settings.AUTH_URL )
+    if not_auth(request):
+        return redirect(settings.AUTH_URL)
     # AUTHENTIFICATION
-    
+
     if request.user.is_authenticated:
-        response = requests.get('http://127.0.0.1:5000/api/subnet')
+        try :
+            response = requests.get('http://127.0.0.1:5000/api/subnet')
+        except ConnectionError as e:
+            return render(request, 'error/Error503.html')
         json_data = response.json()
         if request.method == "POST":
             form = PostForm(request.POST)
             if form.is_valid():
                 route = form.save(commit=False)
-                print (route.ip)
-                print (route.next_hop)
-                print (route.community)
-                
+                requests.post('http://127.0.0.1:5000/api/subnet',
+                              json={
+                                  "ip": str(route.ip),
+                                  "communities": str(route.community),
+                                  "next_hop": str(route.next_hop)})
         else:
             form = PostForm()
-
-        
         return render(request, 'route_dashboard/route_dashboard.html', {
             'data' : json_data,
             'form': form,
@@ -65,10 +63,10 @@ def index(request):
 def change_password(request):
 
     # AUTHENTIFICATION
-    if(not_auth(request)):
-        return redirect( settings.AUTH_URL )
+    if not_auth(request):
+        return redirect(settings.AUTH_URL)
     # AUTHENTIFICATION
-    
+
     if request.method == 'POST':
         form = PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
@@ -83,12 +81,3 @@ def change_password(request):
     return render(request, 'registration/change_password.html', {
         'form': form
     })
-
-
-
-
-
-
-
-
-
