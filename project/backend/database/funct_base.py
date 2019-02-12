d89e30340ede28a44cbc96a05c734e5fa7265fc0
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-02-12 16:50:53 +0100

    database : functions for database update

--- project/backend/database/funct_base.py
+++ project/backend/database/funct_base.py
@@ -1,155 +1,46 @@
+from datetime import datetime
+from pprint import pprint
 from pymongo import MongoClient
-import datetime
 
-client = MongoClient('localhost:27017')
-global dbr
-global dbi
-dbr = client.Route
-dbi = client.Security
-
-def insert():
-    try:
-        routeId = input('Enter Id :')
-        routeNetwork = input('Enter Network :')
-        routeMask = input('Enter Mask :')
-        routeNextHop = input('Enter NextHop :')
-        routeCommunity = input('Enter Community:')
-        
-        dbr.Route.insert_one(
-            {
-                "id": routeId,
-                "network": routeNetwork,
-                "mask": routeMask,
-                "nexthop": routeNextHop,
-                "community": routeCommunity,
-                "enabled": 1,
-     #           "created":datetime.now(),
-     #           "modified":datetime.now(),
-        })
-        print ('\nInserted data successfully\n')
-    
-    except Exception as e:
-        print (str(e))
-
-def read():
-    try:
-        routesCol = dbr.Route.find()
-        print ('\n All data from Route Database \n')
-        for route in routesCol:
-            print (route)
-
-    except Exception as e:
-        print (str(e))
-
-def activate():
-    try:
-        criteria = input('\nEnter id to update\n')
-        routeNetwork = input('\nEnter network to update\n')
-
-        dbr.Route.update_one(
-            {"id": criteria},
-            {
-                "$set": {
-      #              "modified":datetime.now(),
-                    "enabled":0
-                }
-            }
-        )
-        print ("\nRecord updated successfully\n")   
-    
-    except Exception as e:
-        print (str(e))
-
-def desactivate():
-    try:
-        criteria = input('\nEnter id to update\n')
-        qrouteNetwork = input('\nEnter network to update\n')
-
-        dbr.Route.update_one(
-            {"id": criteria},
-            {
-                "$set": {
-       #             "modified":datetime.now(),
-                    "enabled":1
-                }
-            }
-        )
-        print ("\nRecord updated successfully\n")
-    
-    except Exception as e:
-        print (str(e))
-
-def update():
-    selection = input('\nSelect ACT to activate, DESACT to desactivate\n')
-    
-    if selection == 'ACT':
-        activate()
-    elif selection == 'DESACT':
-        desactivate()
-    else:
-        print ('\n INVALID SELECTION \n')
-
-
-def delete():
-    try:
-        criteria = input('\nEnter id to delete\n')
-        dbr.Route.delete_many({"id": criteria})
-        print ('\nDeletion successful\n')
-    except Exception as e:
-        print (str(e))
-
-def register():
-    try:
-        userId = input('Enter Id :')
-        userUsername = input('Enter Username :')
-        userPassword = input('Enter Password :')
-        userEmail = input('Enter Email :')
-        
-        dbi.Security.insert_one(
-            {
-                "id": userId,
-                "username":userUsername,
-                "password":userPassword,
-                "email":userEmail,
-     #           "created":datetime.now(),
-     #           "modified":datetime.now(),
-        })
-        print ('\nRegistered successfully\n')
-    
-    except Exception as e:
-        print (str(e))
-
-def connect():
-    while(1):
-        selection = input('\nSelect POST to insert, PUT to update, to read, DELETE to delete, DECO to disconnect\n')
-        if selection == 'POST':
-            insert()
-        elif selection == 'PUT':
-            update()
-        elif selection == 'GET':
-           	read()
-        elif selection == 'DELETE':
-           	delete()
-        elif selection == 'DECO':
-            print ('\nBye\n')
-            break
+class MongoDB:
+    def __init__(self, collection_name):
+        self.client = MongoClient('mongodb://admin:admin45@ds127115.mlab.com:27115/route_base')
+        self.database = self.client.route_base
+        if collection_name == 'Route' :
+            self.route = self.database.Route
         else:
-            print ('\n INVALID SELECTION \n')
+            self.route = self.database.Test
 
-def login():
-    try:
-        usersCol = dbi.Security.find()
-        #userId = raw_input('Enter Id :')
-        userUsername = input('Enter Username :')
-        userPassword = input('Enter Password :')
-        for users in usersCol:
-            if users == userUsername:
-                if users.password == userPassword:
-                    connect()
-                else:
-                    print ('\nBad password\n')
-            else:
-                print ('\nBad username\n')
+#methods=['GET']
+    def get_all_routes(self):
+        routes = self.route.find()
+        for route in routes:
+            pprint (route) 
 
-    except Exception as e:
-        print (str(e))
+#methods=['POST']
+    def add_route(self, post):
+        route_ip = post['ip']
+        route_nexthop = post['next_hop']
+        route_communities = post['communities']
+        self.route.insert_one({
+            'ip': route_ip,
+            'next_hop': route_nexthop,
+            'communities': route_communities,
+            'created_at': datetime.now(),
+            'modified_at': datetime.now(),
+            'is_activated': 1,
+            'last_activation': datetime.now()})
+
+#methods=['DELETE']
+    def delete_route(self, post):
+        route_ip = post['ip']
+        self.route.delete_many({'ip': route_ip})
+
+#methods=['PATCH']
+    def update_route(self, ip):
+        route_ip = ip['ip']
+        r = self.route.find_one({'ip': route_ip})
+        self.route.update_one({"ip": route_ip},
+                {"$set":{ 'modified_at':datetime.now(),
+                          'is_activated':1-r['is_activated'],
+                          'last_activation': datetime.now()}})
