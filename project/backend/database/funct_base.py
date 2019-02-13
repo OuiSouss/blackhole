4e733e1f85f75ebcafd7603200d3a5b23bcbfbb2
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-02-13 16:25:26 +0100

    database : add doc for funct_base.py

--- project/backend/database/funct_base.py
+++ project/backend/database/funct_base.py
@@ -1,9 +1,22 @@
+"""
+    Database on MongoDB
+"""
+
 from datetime import datetime
 from pprint import pprint
 from pymongo import MongoClient
 
 class MongoDB:
+    """
+	 Class of the database on MongoDB
+    """
+
     def __init__(self, collection_name):
+        """
+         Initialization of the connection to the database
+	     :param collection_name: The name of the collection in the database
+	     :type collection_name: string
+        """
         self.client = MongoClient('mongodb://admin:admin45@ds127115.mlab.com:27115/route_base')
         self.database = self.client.route_base
         if collection_name == 'Route':
@@ -13,12 +26,23 @@ class MongoDB:
 
 #methods=['GET']
     def get_all_routes(self):
+        """
+         Get all of the routes stored in the database
+         :return: A list contains all of the routes
+        """
         routes = self.route.find()
+        output = []
         for route in routes:
             pprint(route)
+            output.append(route)
+        return output
 
 #methods=['POST']
     def add_route(self, post):
+        """
+         Add one route in the database
+         :type post: dict
+        """
         route_ip = post['ip']
         route_nexthop = post['next_hop']
         route_communities = post['communities']
@@ -33,11 +57,20 @@ class MongoDB:
 
 #methods=['DELETE']
     def delete_route(self, post):
+        """
+         Delete one route in the database
+         :type post: dict
+        """
         route_ip = post['ip']
         self.route.delete_many({'ip': route_ip})
 
 #methods=['PATCH']
     def update_route(self, ip):
+        """
+         Update the field 'is_activated'.
+         If the field value is 0, the fied value become 1 (vice versa).
+         :param ip: dict
+        """
         route_ip = ip['ip']
         r = self.route.find_one({'ip': route_ip})
         self.route.update_one({"ip": route_ip},
