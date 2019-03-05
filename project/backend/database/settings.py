69100ccf3d308620b6eb6061ec71a8f4d00973cd
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-03-05 20:27:21 +0100

    Backend : database : add settings.py

---
+++ project/backend/database/settings.py
@@ -0,0 +1,12 @@
+"""
+Database settings for the project.
+Copy and paste your URI database on URI_DB.
+"""
+
+# URI of the database on Mlab website
+URI_DB = 'admin:admin45@ds127115.mlab.com:27115/route_base'
+
+def get_uri():
+    """Get the URI"""
+    return URI_DB
+    
