5f72337b5931108ad29b4c5219ed991fee3c44d1
Kevin Maitre <kevin.maitre@etu.u-bordeaux.fr>
2019-03-12 17:32:10 +0100

    frontend : regex ipv4 + mask

--- project/frontend/route_manager/models.py
+++ project/frontend/route_manager/models.py
@@ -2,11 +2,14 @@
 Define the model used to create a new route.
 """
 from django.db import models
+from django.core.validators import RegexValidator
+
+validator='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(/(3[0-2]|[1-2][0-9]|[0-9]))$'
 
 class Post(models.Model):
     """
     New route model.
     """
-    ip = models.GenericIPAddressField(protocol='IPv4')
-    next_hop = models.GenericIPAddressField(protocol='IPv4')
+    ip = models.CharField(max_length=18, validators=[RegexValidator(validator, message="Exemple: 192.168.0.1/32")])
+    next_hop = models.CharField(max_length=18, validators=[RegexValidator(validator, message="Exemple: 192.168.0.1/32")])
     community = models.CharField(max_length=100)
