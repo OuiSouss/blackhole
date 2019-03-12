346d8c3a28a03d6f6bb94e1b02f6d6b70c6d290c
Geoffroy Gressier <ggressier@emi.u-bordeaux1.fr>
2019-03-12 17:30:56 +0100

    Frontend : Correction of previous test coverage. Forms partially tested, api reachability tested. Frontend API tests are being plannified

--- project/frontend/blackhole_ui/tests.py
+++ project/frontend/blackhole_ui/tests.py
@@ -1,9 +1,3 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from django.conf import settings
-
-from route_manager.forms import PostForm
-
 """
 Tests for the frontend
 
@@ -12,6 +6,14 @@ Elements tested :
 - form validation (add route)
 """
 
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.conf import settings
+import requests
+from requests.exceptions import ConnectionError
+
+from route_manager.forms import PostForm
 
 class LogInTest(TestCase):
     """
@@ -46,7 +48,7 @@ class LogInTest(TestCase):
         """
         response = self.client.post(settings.LOGIN_URL, self.credentials, follow=True)
         self.assertTrue(response.context['user'].is_active)
-    
+
 
     def test_access_restricted_content(self):
         """
@@ -60,7 +62,7 @@ class LogInTest(TestCase):
         response = self.client.post(url_destination, self.credentials)
         self.assertNotEqual(response.url, url_destination)
         self.assertEqual(response.status_code, 302)
-        
+
         # not authenticated already, but redirection should be successful
         response = self.client.post(url_destination, self.credentials, follow=True)
         self.assertEqual(response.status_code, 200)
@@ -68,7 +70,7 @@ class LogInTest(TestCase):
 
         # going to login, should work (and redirect to dashboard)
         response = self.client.post(url_login, self.credentials)
-        self.assertEqual(response.status_code,302)
+        self.assertEqual(response.status_code, 302)
         response = self.client.post(url_login, self.credentials, follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_authenticated)
@@ -83,12 +85,12 @@ class LogInTest(TestCase):
         Test if the user becomes anonymous after a logout
         """
         response = self.client.post(settings.LOGIN_URL, self.credentials, follow=True)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context['user'].is_anonymous)
         self.assertTrue(response.context['user'].is_authenticated)
 
         response = self.client.post(settings.LOGOUT_URL, self.credentials, follow=True)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
         self.assertFalse(response.context['user'].is_authenticated)
 
@@ -139,7 +141,7 @@ class BadLogInTest(TestCase):
         response = self.client.post(url_destination, self.credentials)
         self.assertNotEqual(response.url, url_destination)
         self.assertEqual(response.status_code, 302)
-    
+
         # not authenticated already, but redirection should be successful
         response = self.client.post(url_destination, self.credentials, follow=True)
         self.assertEqual(response.status_code, 200)
@@ -147,9 +149,9 @@ class BadLogInTest(TestCase):
 
         # going to login, should fail (and not redirect to dashboard)
         response = self.client.post(url_login, self.credentials)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         response = self.client.post(url_login, self.credentials, follow=True)
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
         self.assertFalse(response.context['user'].is_authenticated)
 
         # attempt to access restricted content, should refuse and redirect
@@ -157,21 +159,73 @@ class BadLogInTest(TestCase):
         self.assertEqual(response.status_code, 302)
 
 
+#clear && python3 manage.py test blackhole_ui.tests.APIbackendTest
+class APIbackendTest(TestCase):
+    """
+    Test backend API
+    """
+
+    def test_reachable_api(self):
+        """
+        Test if the API is reachable
+
+        Exception when unavailable :
+        <class 'requests.exceptions.ConnectionError'>
+
+        The standard "ConnectionError" doesnt work
+        """
+        try:
+            requests.get(settings.API_URL)
+
+        except ConnectionError:
+            raise AssertionError("\n>>> Backend API unreachable, consider enabling it")
+
+
+    def test_get_routes(self):
+        """
+        Test to get the routes
+        """
+
+        try:
+            response = requests.get(settings.API_URL)
+            json_data = response.json()
+
+            self.assertGreaterEqual(len(json_data), 0)
+
+        except ConnectionError:
+            raise AssertionError("\n>>> Backend API unreachable, consider enabling it")
+
+
+    #def test_add_routes(self):
+    #def test_delete_routes(self):
+    #def test_activate_routes(self):
+    #def test_desactivate_routes(self):
+    #def test_modify_routes(self):
+
+
+#clear && python3 manage.py test blackhole_ui.tests.RouteManagerTest
 class RouteManagerTest(TestCase):
-    #clear && python3 manage.py test blackhole_ui.tests.RouteManagerTest
+    """
+    Test elements of the route manager
+    """
 
     def test_form_add_route(self):
-
+        """
+        Test the form to add a route, with correct elements
+        """
         form_data = {}
         form_data['ip'] = '192.168.1.1'
         form_data['community'] = '1234x'
         form_data['next_hop'] = '0.0.0.0'
 
-        form = PostForm(data = form_data)
+        form = PostForm(data=form_data)
         self.assertTrue(form.is_valid())
 
 
     def get_incorrect_ip_adress_list(self):
+        """
+        Create a non-exhaustive list of incorrect ip adress to test
+        """
         wrong_ip_adress_list = []
         wrong_ip_adress_list.clear()
         wrong_ip_adress_list.append('wrong')
@@ -188,7 +242,11 @@ class RouteManagerTest(TestCase):
         return wrong_ip_adress_list
 
 
+
     def test_form_add_route_incorrect_ip(self):
+        """
+        Test the form to add a route, but with a list of incorrect ip
+        """
 
         wrong_ip_list = self.get_incorrect_ip_adress_list()
 
@@ -196,13 +254,13 @@ class RouteManagerTest(TestCase):
         form_data['community'] = '1234x'
         form_data['next_hop'] = '0.0.0.0'
 
-        for wrong_ip in wrong_ip_list:    
+        for wrong_ip in wrong_ip_list:
             form_data['ip'] = wrong_ip
-            form = PostForm(data = form_data)
+            form = PostForm(data=form_data)
 
             # Testing impossible cases
             # the form must not become valid
-            if(form.is_valid()):
+            if form.is_valid():
                 # If a wrong value is accepted as correct
                 # Then an assert is triggered
                 print(">> accepted wrong_ip : ", wrong_ip)
@@ -210,6 +268,9 @@ class RouteManagerTest(TestCase):
 
 
     def test_form_add_route_incorrect_community(self):
+        """
+        Test the form to add a route, but with a list of incorrect community
+        """
 
         wrong_community_list = []
         wrong_community_list.append(None)
@@ -219,13 +280,13 @@ class RouteManagerTest(TestCase):
         form_data['ip'] = '192.168.1.1'
         form_data['next_hop'] = '0.0.0.0'
 
-        for wrong_community in wrong_community_list:    
+        for wrong_community in wrong_community_list:
             form_data['community'] = wrong_community
-            form = PostForm(data = form_data)
+            form = PostForm(data=form_data)
 
             # Testing impossible cases
             # the form must not become valid
-            if(form.is_valid()):
+            if form.is_valid():
                 # If a wrong value is accepted as correct
                 # Then an assert is triggered
                 print(">> accepted community : ", wrong_community)
@@ -233,6 +294,9 @@ class RouteManagerTest(TestCase):
 
 
     def test_form_add_route_incorrect_next_hop(self):
+        """
+        Test the form to add a route, but with a list of incorrect next_hop
+        """
 
         wrong_next_hop_list = self.get_incorrect_ip_adress_list()
 
@@ -240,15 +304,14 @@ class RouteManagerTest(TestCase):
         form_data['ip'] = '192.168.1.1'
         form_data['community'] = '1234x'
 
-        for wrong_next_hop in wrong_next_hop_list:    
+        for wrong_next_hop in wrong_next_hop_list:
             form_data['next_hop'] = wrong_next_hop
-            form = PostForm(data = form_data)
+            form = PostForm(data=form_data)
 
             # Testing impossible cases
             # the form must not become valid
-            if(form.is_valid()):
+            if form.is_valid():
                 # If a wrong value is accepted as correct
                 # Then an assert is triggered
                 print(">> accepted next_hop : ", wrong_next_hop)
                 self.assertFalse(form.is_valid())
-
