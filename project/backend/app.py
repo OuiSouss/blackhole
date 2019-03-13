a1b8ae0321be3519f378596522c7045cf274d78e
Cyrielle D <cyrielle.dubois@etu.u-bordeaux.fr>
2019-03-13 11:33:15 +0100

    backend : add new resource but in comment cause no implemented

--- project/backend/app.py
+++ project/backend/app.py
@@ -8,6 +8,7 @@ from flask_restful import Api
 
 from backend.resources.subnet import Subnet
 from backend.resources.subnets import Subnets
+#from backend.resources.exabgp import ExaBGP
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -15,3 +16,4 @@ api = Api(api_bp)
 # Route
 api.add_resource(Subnet, '/subnet/<object_id:subnet_id>')
 api.add_resource(Subnets, '/subnets')
+#api.add_resource(ExaBGP, '/exabgp')
