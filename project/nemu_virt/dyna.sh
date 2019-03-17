73ddf56bfbea3f6546f894c13f407b46f32f5a97
Amélie Risi <amelie.risi@etu.u-bordeaux.fr>
2019-03-18 00:38:17 +0100

    nemu_virt : Remove br cisco router

--- project/nemu_virt/dyna.sh
+++ project/nemu_virt/dyna.sh
@@ -18,9 +18,3 @@ echo "lauching router 2"
 $DYNA -P 7200 --idle-pc $IDLE_PC -i 2 -X -T 2002 \
  -p 1:PA-FE-TX -s 1:0:udp:10005:127.0.0.1:10004 -p 2:PA-FE-TX -s 2:0:udp:10009:127.0.0.1:10008 \
  $CISCO_IMAGE -C $R2_cfg &
-
-echo "launching border router"
-
-$DYNA -P 7200 --idle-pc $IDLE_PC -i 3 -X -T 2003 \
- -p 1:PA-FE-TX -s 1:0:udp:11010:127.0.0.1:11009 -p 2:PA-FE-TX -s 2:0:udp:11016:127.0.0.1:11015 \
- $CISCO_IMAGE -C $BR_cfg &
