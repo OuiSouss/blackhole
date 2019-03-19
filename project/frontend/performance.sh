983058b893c3897ecdc2d03cbd01494d5bbc028e
Geoffroy Gressier <ggressier@emi.u-bordeaux1.fr>
2019-03-19 19:26:27 +0100

    Test : bash file to test performances, not meant to be used by the end-user but still present if needed

--- project/frontend/performance.sh
+++ project/frontend/performance.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-clear
+#clear
 
-python3 manage.py test blackhole_ui.tests.APIbackendTest.perf_route_creation > /dev/null 2> tmp.txt
-cat tmp.txt | grep "Ran" | cut -c15-
+#python3 manage.py test blackhole_ui.tests.APIbackendTest.perf_route_creation > /dev/null 2> tmp.txt
+#cat tmp.txt | grep "Ran" | cut -c15-
 
 #python3 manage.py test blackhole_ui.tests.APIbackendTest.perf_route_deletion > /dev/null 2> tmp.txt
 #cat tmp.txt | grep "Ran" | cut -c15-
 
-python3 manage.py test blackhole_ui.tests.APIbackendTest.perf_route_destruction > /dev/null 2> tmp.txt
-cat tmp.txt | grep "Ran" | cut -c15-
+#python3 manage.py test blackhole_ui.tests.APIbackendTest.perf_route_destruction > /dev/null 2> tmp.txt
+#cat tmp.txt | grep "Ran" | cut -c15-
 
-rm tmp.txt
+#rm tmp.txt
