4f971aa64b9d23101dfb7b153a56c8c77cad23dc
Kevin Maitre <kevin.maitre@etu.u-bordeaux.fr>
2019-03-28 13:57:44 +0100

    frontend : community nullable

--- project/frontend/route_manager/models.py
+++ project/frontend/route_manager/models.py
@@ -16,4 +16,4 @@ class Post(models.Model):
         validator_ip, message="Exemple: 192.168.0.1/32")])
     next_hop = models.GenericIPAddressField(protocol='IPv4')
     community = models.CharField(max_length=5, validators=[RegexValidator(
-        validator_com, message="Exemple: 45:50")])
+        validator_com, message="Exemple: 45:50")], blank=True, null=True)
